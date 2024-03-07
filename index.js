b7eff61a6552f9382e3be3508573aac917109161
Denzel A <124642669+denzelali@users.noreply.github.com>
2024-03-07 15:11:12 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+
+
+const EmployeeRoute = require('./Routes/Employee.route');
+app.use(('/employee'), EmployeeRoute);
+
+const UserRoute =require( './Routes/Users.route')  ;
+app.use(( '/user' ), UserRoute)
+
+//Error if wrong endpoint
+app.use((req, res, next) => {
+    const err = new Error("Not Fount");
+    err.status = 404;
+    next(err);
+});
+
+//Middleware Express Handler
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.send({
+        error: {
+            status: err.status || 500,
+            message: err.message
+        }
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000...');
+});

91be631b1aead853cad76cdd6d5d861fa025f8cd
Denzel A <124642669+denzelali@users.noreply.github.com>
2024-03-07 16:18:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const app = express();
 
+//mongodb+srv://<username>:<password>@aliwate.cuziqf3.mongodb.net/?retryWrites=true&w=majority&appName=aliwate
+//username: ustp
+//pass: user1234
+mongoose.connect('mongodb+srv://<username>:<password>@aliwate.cuziqf3.mongodb.net/?retryWrites=true&w=majority&appName=aliwate').
+then(()=>{
+    console.log('Connected to MongoDB...');
+}).catch((err) => {
+    console.error('Error connecting to MongoDB:', err.message);
+});
+
+
+// const employ = {
+//     employee: [
+//         {id: 1, name: 'Alejandro'},
+//         {id: 2, name: 'Martin'},
+//         {id: 3, name: 'Denis'},
+//         {id: 4, name: 'Castro'}
+//         ]};
 
 const EmployeeRoute = require('./Routes/Employee.route');
 app.use(('/employee'), EmployeeRoute);
